acd0c3acfb137497836cb68b987adbe58a1321b6
Emilia <emiliaprzybylek@tutamail.com>
2022-05-27 13:20:24 +0200

    js: varibles added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const imgBall = document.querySelector('.ball-img');
+const question = document.querySelector('input');
+const answer = document.querySelector('.answer');
+const error = document.querySelector('.error');
